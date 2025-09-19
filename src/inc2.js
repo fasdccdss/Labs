689ea87f8f307fda48af01da43f05a67d1461225
fasdccdss <uskanovtimur@gmail.com>
2025-09-19 04:17:07 +0300

    Delete src directory

--- src/inc2.js
+++
@@ -1,9 +0,0 @@
-const inc = (num) => {
-    if (typeof num.n !== "number") throw new Error (" can't increase a non-numeric variable");
-    num.n = num.n + 1;
-}
-
-const obj = { n: 5 };
-inc(obj);
-
-console.dir(obj);
